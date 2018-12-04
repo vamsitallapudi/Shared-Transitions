8a06465c79a558d907a9546a1b52daa1f78b996b
Vamsi Tallapudi <vtallapudi@processmap.com>
2018-12-04 19:10:20 +0530

    few changes

--- app/src/main/java/com/coderefer/sharedtransitions/MainActivity.kt
+++ app/src/main/java/com/coderefer/sharedtransitions/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityOptionsCompat
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewCompat
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,8 +15,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.new_bg))
         imageView.setOnClickListener {startDetailActivity(imageView)}
         imageView.tag = "new_bg"
+        imageView2.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.apple_raw))
         imageView2.setOnClickListener {startDetailActivity(imageView2)}
         imageView2.tag = "apple_raw"
     }
